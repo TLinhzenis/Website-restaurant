3fef319678e5be674df6a3baad8d8b985c454df2
TLinhzenis <btuanlinh715@gmail.com>
2024-12-23 23:03:42 +0700

    final

--- restoran-1.0.0/js/QLThongKe.js
+++ restoran-1.0.0/js/QLThongKe.js
@@ -80,10 +80,10 @@ async function fetchRevenue() {
 
       // Cập nhật dữ liệu vào HTML
 
-      document.getElementById('dailyRevenue').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.today.totalRevenue.toLocaleString()}</span> VND`;
-      document.getElementById('dailyRevenue1').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.today.totalRevenue.toLocaleString()}</span> VND`;
-      document.getElementById('7daysRevenue').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.last7Days.totalRevenue.toLocaleString()}</span> VND`;
-      document.getElementById('30daysRevenue').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.last30Days.totalRevenue.toLocaleString()}</span> VND`;
+      document.getElementById('dailyRevenue').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.today.totalRevenue.toLocaleString()} VND</span> `;
+      document.getElementById('dailyRevenue1').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.today.totalRevenue.toLocaleString()} VND</span> `;
+      document.getElementById('7daysRevenue').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.last7Days.totalRevenue.toLocaleString()} VND</span> `;
+      document.getElementById('30daysRevenue').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.last30Days.totalRevenue.toLocaleString()} VND</span> `;
       
     } catch (error) {
       console.error('Không thể tải dữ liệu doanh số.', error);
@@ -104,10 +104,10 @@ async function fetchRevenue() {
 
       // Cập nhật dữ liệu vào HTML
 
-      document.getElementById('dailyOrders').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.today.orderCount.toLocaleString()}</span> `;
-      document.getElementById('dailyOrders1').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.today.orderCount.toLocaleString()}</span> `;
-      document.getElementById('7daysOrders').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.last7Days.orderCount.toLocaleString()}</span> `;
-      document.getElementById('30daysOrders').innerHTML = `<span style="color: #fea116;padding-left:10px;">${data.last30Days.orderCount.toLocaleString()}</span> `;
+      document.getElementById('dailyOrders').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.today.orderCount.toLocaleString()}</span> `;
+      document.getElementById('dailyOrders1').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.today.orderCount.toLocaleString()}</span> `;
+      document.getElementById('7daysOrders').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.last7Days.orderCount.toLocaleString()}</span> `;
+      document.getElementById('30daysOrders').innerHTML = `<span style="color: #076d7d;padding-left:10px;">${data.last30Days.orderCount.toLocaleString()}</span> `;
       
     } catch (error) {
       console.error('Không thể tải dữ liệu đơn hàng', error);
@@ -133,14 +133,24 @@ async function fetchRevenue() {
         }
 
         // Cập nhật thông tin vào giao diện HTML
-        document.getElementById('top1Customer').innerHTML = 
-            `${data[0].fullName} <span style="color: #fea116;padding-left:10px;">(${data[0].totalSpent.toLocaleString()} VND)</span>`;
+        document.getElementById('top1CustomerName').innerHTML = 
+            `<span>${data[0].fullName}</span>`
+        document.getElementById('top1Customer').innerHTML =
+            `<span >${data[0].totalSpent.toLocaleString()} VND</span>`;
+       
         document.getElementById('top1Customer1').innerHTML = 
-            `<span style="padding-left:10px;">${data[0].fullName}</span> <span style="color: #fea116;padding-left:10px;">(${data[0].totalSpent.toLocaleString()} VND)</span>`;
-        document.getElementById('top2Customer').innerHTML = 
-            `${data[1].fullName} <span style="color: #fea116;padding-left:10px;">(${data[1].totalSpent.toLocaleString()} VND)</span>`;
-        document.getElementById('top3Customer').innerHTML = 
-            `${data[2].fullName} <span style="color: #fea116;padding-left:10px;">(${data[2].totalSpent.toLocaleString()} VND)</span>`;
+            `<span style="padding-left:10px;">${data[0].fullName}</span> 
+            <span style="color: #076d7d;padding-left:10px;">(${data[0].totalSpent.toLocaleString()} VND)</span>`;
+        
+        document.getElementById('top2CustomerName').innerHTML = 
+            `<span>${data[1].fullName}</span>`
+        document.getElementById('top2Customer').innerHTML =
+            `<span >${data[1].totalSpent.toLocaleString()} VND</span>`;
+
+        document.getElementById('top3CustomerName').innerHTML = 
+            `<span>${data[2].fullName}</span>`
+        document.getElementById('top3Customer').innerHTML =
+            `<span >${data[2].totalSpent.toLocaleString()} VND</span>`;
     } catch (error) {
         console.error('Không thể tải dữ liệu khách hàng hàng đầu.', error);
     }
@@ -164,16 +174,30 @@ async function fetchRevenue() {
         }
 
         // Cập nhật thông tin vào giao diện HTML
-        document.getElementById('top1Menu').innerHTML = 
-            `${data[0].itemName} <span style="color: #fea116;padding-left:10px;">(${data[0].totalQuantity.toLocaleString()} phần)</span>`;
-        document.getElementById('top2Menu').innerHTML = 
-            `${data[1].itemName} <span style="color: #fea116;padding-left:10px;">(${data[1].totalQuantity.toLocaleString()} phần)</span>`;
-        document.getElementById('top3Menu').innerHTML = 
-            `${data[2].itemName} <span style="color: #fea116;padding-left:10px;">(${data[2].totalQuantity.toLocaleString()} phần)</span>`;
-        document.getElementById('top4Menu').innerHTML = 
-            `${data[3].itemName} <span style="color: #fea116;padding-left:10px;">(${data[3].totalQuantity.toLocaleString()} phần)</span>`;
-        document.getElementById('top5Menu').innerHTML = 
-            `${data[4].itemName} <span style="color: #fea116;padding-left:10px;">(${data[4].totalQuantity.toLocaleString()} phần)</span>`;
+        document.getElementById('top1MenuName').innerHTML = 
+            `<span>${data[0].itemName}</span>` 
+        document.getElementById('top1Menu').innerHTML =
+            `<span>${data[0].totalQuantity.toLocaleString()} phần</span>`;
+
+        document.getElementById('top2MenuName').innerHTML = 
+            `<span>${data[1].itemName}</span>` 
+        document.getElementById('top2Menu').innerHTML =
+            `<span>${data[1].totalQuantity.toLocaleString()} phần</span>`;
+
+        document.getElementById('top3MenuName').innerHTML = 
+            `<span>${data[2].itemName}</span>` 
+        document.getElementById('top3Menu').innerHTML =
+            `<span>${data[2].totalQuantity.toLocaleString()} phần</span>`;
+
+        document.getElementById('top4MenuName').innerHTML = 
+            `<span>${data[3].itemName}</span>` 
+        document.getElementById('top4Menu').innerHTML =
+            `<span>${data[3].totalQuantity.toLocaleString()} phần</span>`;
+
+        document.getElementById('top5MenuName').innerHTML = 
+            `<span>${data[4].itemName}</span>` 
+        document.getElementById('top5Menu').innerHTML =
+            `<span>${data[4].totalQuantity.toLocaleString()} phần</span>`;
     } catch (error) {
         console.error('Không thể tải dữ liệu món ăn hàng đầu.', error);
  
